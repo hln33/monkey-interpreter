08327b92ac358ba8cc9ed78faaf11074c45790aa
Harry Nguyen <hln5@sfu.ca>
2025-05-23 20:01:42 -0700

    evaluate in repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		eval := evaluator.Eval(program)
+		if eval != nil {
+			io.WriteString(out, eval.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
