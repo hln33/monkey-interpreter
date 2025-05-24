63f98fe13282a43afe5261040d766c84c3e0c102
Harry Nguyen <hln5@sfu.ca>
2025-05-23 20:10:12 -0700

    add support for evaluating booleans

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -30,17 +30,46 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	return true
 }
 
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	res, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if res.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			res.Value, expected)
+		return false
+	}
+	return true
+}
+
 func TestIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected int64
 	}{
-		{"5", 4},
+		{"5", 5},
 		{"10", 10},
 	}
 
 	for _, test := range tests {
 		eval := testEval(test.input)
 		testIntegerObject(t, eval, test.expected)
+	}
+}
+
+func TestBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, test := range tests {
+		eval := testEval(test.input)
+		testBooleanObject(t, eval, test.expected)
 	}
 }
